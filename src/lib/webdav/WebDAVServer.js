8b1dd088b3becb8b80b0e80bd837631ab4c4ba06
goodbyepavlyi <goodbyepavlyi@gmail.com>
2024-07-29 18:14:32 +0200

    refactor webdavserver's https implementation

--- src/lib/webdav/WebDAVServer.js
+++ src/lib/webdav/WebDAVServer.js
@@ -4,7 +4,6 @@ const webdav = require("webdav-server").v2;
 const Logger = require("../Logger");
 const DiscordFilesystem = require("../storage/DiscordFilesystem");
 const Config = require("../Config");
-const Utils = require("../utils/Utils");
 
 module.exports = class WebDAVServer {
     /**
@@ -19,23 +18,19 @@ module.exports = class WebDAVServer {
         };
 
         if (Config.webserverEnableHttps) {
-            Logger.info(Logger.Type.WebDAV, "Enabling HTTPS");
-
             if (!fs.existsSync(path.resolve(__dirname, "../../data/certs/privkey.pem"))) {
-                Logger.error(Logger.Type.WebDAV, `No private key found at &c./data/certs/privkey.pem&r. Please generate a self-signed certificate or disable HTTPS.`);
-                Logger.info(Logger.Type.WebDAV, `You can generate a self-signed certificate using &copenssl req -x509 -newkey rsa:4096 -keyout privkey.pem -out cert.pem -days 365&r`);
+                Logger.error(Logger.Type.WebDAV, `No private key found at &c./data/certs/privkey.pem&r, please &cgenerate a self-signed certificate&r or &cdisable HTTPS&r.`);
                 return process.exit(1);
             }
 
             if (!fs.existsSync(path.resolve(__dirname, "../../data/certs/cert.pem"))) {
-                Logger.error(Logger.Type.WebDAV, `No certificate found at &c./data/certs/cert.pem&r. Please generate a self-signed certificate or disable HTTPS.`);
-                Logger.info(Logger.Type.WebDAV, `You can generate a self-signed certificate using &copenssl req -x509 -newkey rsa:4096 -keyout privkey.pem -out cert.pem -days 365&r`);
+                Logger.error(Logger.Type.WebDAV, `No certificate found at &c./data/certs/cert.pem&r, please &cgenerate a self-signed certificate&r or &cdisable HTTPS&r.`);
                 return process.exit(1);
             }
 
             this.serverOptions.https = {
-                key: Utils.readFileSyncOrUndefined(path.resolve(__dirname, "../../data/certs/privkey.pem")),
-                cert: Utils.readFileSyncOrUndefined(path.resolve(__dirname, "../../data/certs/cert.pem"))
+                key: fs.readFileSync(path.resolve(__dirname, "../../data/certs/privkey.pem")),
+                cert: fs.readFileSync(path.resolve(__dirname, "../../data/certs/cert.pem"))
             }
         }
 
